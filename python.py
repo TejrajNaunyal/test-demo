b804a2b0ff674c9c50387a73cbfcdc15b2793f8c
tekrajnaunyal372@gmail.com <tekrajnaunyal372@gmail.com>
2023-11-29 07:29:52 +0545

    commited

--- python.py
+++ python.py
@@ -7,3 +7,8 @@ def factorial(n):
 number = int(input("Enter a number: "))
 result = factorial(number)
 print(f"The factorial of {number} is {result}")
+def factorial(n):
+    if n==1:
+        return 1
+    else:
+        return n * factorial(n-1)
